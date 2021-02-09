6c6d52b81fc00a617f5839de1f995b0fd88b48d4
Anurag <vizculus@gmail.com>
2021-02-09 12:42:22 +0545

    Added hamburger menu and made some changes to the streambuilder that handles sign in

--- lib/normalscreen.dart
+++ lib/normalscreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class normalscreen extends StatefulWidget{
   @override
@@ -13,7 +12,7 @@ class _normalscreenState extends State<normalscreen>{
   Widget build(BuildContext context){
     return Column(
       children: <Widget>[
-	SizedBox(height: 700),
+	SizedBox(height: 200),
 	RaisedButton(
 	  child: Text(
 	    'Logout',
--- lib/userscreen.dart
+++ lib/userscreen.dart
@@ -11,20 +11,39 @@ class userscreen extends StatelessWidget{
       stream: FirebaseFirestore.instance.collection('token').doc(FirebaseAuth.instance.currentUser.uid).snapshots(),
       builder: (context, snapshot){
 	if(snapshot.connectionState == ConnectionState.waiting){
-	  return Container();
+	  return Container(color: Color(0xff0e0f26),);
         }
 	else{
-	  return Scaffold(
-	    appBar: AppBar(
-	      title: Center(child: Text("batuwa", style: TextStyle(fontFamily: 'Mohave',))),
-	      backgroundColor: Color(0xff252a42),
-	    ),
-	    body: Container(
-	      alignment: Alignment.center,
-	      color: Color(0xff0e0f26),
-	      child:(snapshot.data['token'] == 1) ? firsttimeLogin() : normalscreen(),
-	    ),
-	  );
+	  if(snapshot.data['token'] == 1){
+	    return Scaffold(
+	      appBar: AppBar(
+		title: Center(child: Text("batuwa", style: TextStyle(fontFamily: 'Mohave',))),
+		backgroundColor: Color(0xff252a42),
+	      ),
+	      body: Container(
+		alignment: Alignment.center,
+		color: Color(0xff0e0f26),
+		child: firsttimeLogin(),
+	      ),
+	    );
+	  }
+	  else{
+	    return Scaffold(
+	      drawer: Drawer(
+		child: ListView(
+		),
+	      ),
+	      appBar: AppBar(
+		title: Container(alignment: Alignment(-0.21, 0), child: Text("batuwa", style: TextStyle(fontFamily: 'Mohave',))),
+		backgroundColor: Color(0xff252a42),
+	      ),
+	      body: Container(
+		alignment: Alignment.center,
+		color: Color(0xff0e0f26),
+		child: normalscreen(),
+	      ),
+	    );
+  	  }
         }
       },
     );
