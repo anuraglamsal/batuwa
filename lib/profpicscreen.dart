84c18acab0933ff99a31bdca5d13ac268aeb093b
Anurag <vizculus@gmail.com>
2021-02-16 12:39:01 +0545

    upload profile picture done

--- lib/profpicscreen.dart
+++ lib/profpicscreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
 
 class profpicscreen extends StatefulWidget{
   @override
@@ -10,6 +12,12 @@ class profpicscreen extends StatefulWidget{
 
 class _profpicscreenState extends State<profpicscreen>{
   String imageUrl;
+  bool indicator = false;
+  @override
+  void initState(){
+    super.initState();
+    getImageUrl();
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -23,8 +31,12 @@ class _profpicscreenState extends State<profpicscreen>{
 	child: Column(
 	  children: [
 	    SizedBox(height: 30),
-	    (imageUrl !=null) ?
-	    	Image.network(imageUrl) :
+	    (imageUrl != null) ?
+		CircleAvatar(
+		  radius: 50,
+		  backgroundColor: Color(0xff252a42),
+		  backgroundImage: NetworkImage(imageUrl),
+		) :
 		CircleAvatar(
 		  radius: 50,
 		  backgroundColor: Color(0xff252a42),
@@ -33,25 +45,58 @@ class _profpicscreenState extends State<profpicscreen>{
 		  ),
 		),
             SizedBox(height: 30),	
-	    Container(
-	      height: 50,
-	      width: 210,
-	      child: ElevatedButton(
-		style: ElevatedButton.styleFrom(
-		  primary: Color(0xff252a42),
+	    indicator ?
+	        CircularProgressIndicator(
+		  valueColor: AlwaysStoppedAnimation<Color>(Colors.blueGrey),
+		) : 
+		Container(
+		  height: 50,
+		  width: 210,
+		  child: ElevatedButton(
+		    style: ElevatedButton.styleFrom(
+		      primary: Color(0xff252a42),
+		    ),
+		    onPressed: (){
+		      setState((){
+			indicator = true;
+		      });
+		      uploadImage();
+		    },
+		    child: Text("Upload your profile pic", style: TextStyle(fontFamily: "Mohave", fontSize: 20,)),
+		  ),
 		),
-		onPressed: (){},
-		child: Text("Upload your profile pic", style: TextStyle(fontFamily: "Mohave", fontSize: 20,)),
-	      ),
-	    ),
 	  ],
 	),
       ),
     );
   }
 
-  uploadImage(){
+  uploadImage() async{
+    final _storage = firebase_storage.FirebaseStorage.instance;
+    final _picker = ImagePicker();
+    PickedFile image;
+    await Permission.photos.request();
+    var permissionStatus = await Permission.photos.status;
+    if(permissionStatus.isGranted){
+      image = await _picker.getImage(source: ImageSource.gallery);
+      var file = File(image.path);
+      if(image != null){
+	var snapshot = await _storage.ref()
+	    .child('ProfPic/${FirebaseAuth.instance.currentUser.uid.toString()}')
+	    .putFile(file);
+	await getImageUrl();
+	print(imageUrl);
+	setState((){
+	  indicator = false;
+        });
+      }
+    }
+  }
 
+  getImageUrl() async{
+    imageUrl = await firebase_storage.FirebaseStorage.instance.ref('ProfPic/${FirebaseAuth.instance.currentUser.uid.toString()}').getDownloadURL();
+    setState((){
+    });
   }
 
 }
